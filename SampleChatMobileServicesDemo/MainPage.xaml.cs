b086e81d3f8c004a75ad09b633bae04584dc7b0d
gcramarossa <giuseppe.cramarossa@hotmail.it>
2016-02-16 00:40:42 +0100

    Initial Commit

---
+++ SampleChatMobileServicesDemo/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.WindowsAzure.MobileServices;
+// Il modello di elemento per la pagina vuota è documentato all'indirizzo http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x410
+
+namespace SampleChatMobileServicesDemo
+{
+    /// <summary>
+    /// Pagina vuota che può essere utilizzata autonomamente oppure esplorata all'interno di un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MobileServiceClient _client = new MobileServiceClient("https://provachat.azurewebsites.net");
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void sendText_Click(object sender, RoutedEventArgs e)
+        {
+            Message message = new Message
+            {
+                User = username.Text,
+                Text = textUser.Text
+            };
+
+            messages.Text += username.Text + " : " + textUser.Text + Environment.NewLine;
+
+            _client.GetTable<Message>().InsertAsync(message);
+        }
+
+        private async void messages_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Message> getMessages = await _client.GetTable<Message>().ToListAsync();
+            Message[] savedMessages = getMessages.ToArray();
+            foreach (var messageItem in savedMessages)
+            {
+                messages.Text = messageItem.User + " : " + messageItem.Text + Environment.NewLine;
+            }
+        }
+    }
+}
---
+++ SampleChatMobileServicesDemo/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleChatMobileServicesDemo
+{
+    class Message
+    {
+        public string Id;
+        public string User;
+        public string Text;
+    }
+}
